c1b2718f31dc3345109bd45a07ff22c6ea6e1036
Higor Menezes <higor.menezes@e-deploy.com.br>
2018-10-11 06:53:11 -0400

    start

---
+++ core/core.js
@@ -0,0 +1,11 @@
+
+_FRAME_RATE_ = 60;
+
+class Core {
+    constructor() {
+        setInterval(this.update, 1)
+    }
+
+    update() {
+    }
+}
---
+++ game/player.js
@@ -0,0 +1,5 @@
+class Player extends Core{
+    update (){
+        console.log("Player");
+    }
+}
